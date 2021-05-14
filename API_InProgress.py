c0c51b097e6c58db4b27380af5aca686b6e8865b
FluffyCrocodile <68883661+FluffyCrocodile@users.noreply.github.com>
2021-05-14 14:41:10 -0700

    Update API_InProgress.py

--- API_InProgress.py
+++ API_InProgress.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
+import base64
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,20 +12,31 @@ class StudentsList(Resource):
       return STUDENTS
   def post(self):
       parser.add_argument("name")
-      parser.add_argument("age")
-      parser.add_argument("spec")
-      parser.add_argument("monkey")
+      parser.add_argument("key")
       args = parser.parse_args()
       student_id = int(max(STUDENTS.keys())) + 1
       student_id = '%i' % student_id
+
+      msg = args["name"]
+      key = args["key"]
+
+      enc = []
+      for i in range(len(msg)):
+          key_c = key[i % len(key)]
+          enc_c = chr((ord(msg[i]) +
+                       ord(key_c)) % 256)
+          enc.append(enc_c)
+          print("enc:", enc)
+      encoded = base64.urlsafe_b64encode("".join(enc).encode()).decode()
+
       STUDENTS[student_id] = {
-          "name": args["name"],
-          "age": args["age"]*2,
-          "spec": args["spec"],
-          "Anime": args["monkey"],
+          "name": encoded,
+          "Anime": args["key"]
       }
       nekozilla = "monkey" * 2
       return STUDENTS[student_id], 201
+
+
 
 STUDENTS = {
   '1': {'name': 'Mark', 'age': 23, 'spec': 'math', 'anime': 'Mirai Nikki'},
